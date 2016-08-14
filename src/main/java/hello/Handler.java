5eee8a230761db44c08ba4888aa09e545507ad1d
kaklakariada <kaklakariada@users.noreply.github.com>
2016-08-14 16:01:09 +0200

    Add .editorconfig, format source

--- src/main/java/hello/Handler.java
+++ src/main/java/hello/Handler.java
@@ -7,9 +7,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Handler implements RequestHandler<Request, Response> {
 
-	@Override
-	public Response handleRequest(Request input, Context context) {
-		return new Response("Go Serverless v1.0! Your function executed successfully!", input);
-	}
+    @Override
+    public Response handleRequest(Request input, Context context) {
+        return new Response("Go Serverless v1.0! Your function executed successfully!", input);
+    }
 
 }
--- src/main/java/hello/Response.java
+++ src/main/java/hello/Response.java
@@ -2,31 +2,31 @@ package hello;
 
 public class Response {
 
-	private String message;
-	private Request request;
+    private String message;
+    private Request request;
 
-	public Response(String message, Request request) {
-		this.message = message;
-		this.request = request;
-	}
+    public Response(String message, Request request) {
+        this.message = message;
+        this.request = request;
+    }
 
-	public Response() {
-	}
+    public Response() {
+    }
 
-	public String getMessage() {
-		return this.message;
-	}
+    public String getMessage() {
+        return this.message;
+    }
 
-	public Request getRequest() {
-		return this.request;
-	}
+    public Request getRequest() {
+        return this.request;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public void setRequest(Request request) {
-		this.request = request;
-	}
+    public void setRequest(Request request) {
+        this.request = request;
+    }
 
 }
