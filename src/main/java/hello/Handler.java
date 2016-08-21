c6ee736c1f448e61162a7919978fec865c6c42a8
kaklakariada <kaklakariada@users.noreply.github.com>
2016-08-21 12:43:09 +0200

    Measure background job scheduling

--- src/main/java/hello/Handler.java
+++ src/main/java/hello/Handler.java
@@ -1,15 +1,58 @@
 package hello;
 
-import java.io.IOException;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import hello.Response.Invocation;
+
 public class Handler implements RequestHandler<Request, Response> {
 
-    @Override
-    public Response handleRequest(Request input, Context context) {
-        return new Response("Go Serverless v1.0! Your function executed successfully!", input);
-    }
+	private static final Logger LOG = LoggerFactory.getLogger(Handler.class);
+	private final ScheduledExecutorService executor;
+	private final Instant startup;
 
+	private Invocation previousInvocation;
+
+	private final Clock clock;
+	private int handlerCount = 0;
+	private final ConcurrentLinkedQueue<Invocation> invocations = new ConcurrentLinkedQueue<>();
+
+	public Handler() {
+		LOG.info("Initializing handler");
+		clock = Clock.systemUTC();
+		startup = clock.instant();
+		executor = Executors.newSingleThreadScheduledExecutor();
+		executor.scheduleAtFixedRate(() -> {
+			final Instant now = clock.instant();
+			previousInvocation = new Invocation(invocations.size(), now, previousInvocation);
+			LOG.debug("Thread {}, alive for {}, invocations: {}, current: {}", Thread.currentThread().getName(),
+					getTimeSinceStartup(), invocations.size(), previousInvocation);
+			invocations.add(previousInvocation);
+		}, 0, 1, TimeUnit.SECONDS);
+	}
+
+	@Override
+	public Response handleRequest(Request input, Context context) {
+		handlerCount++;
+		final String message = "Request #" + handlerCount + ", sime since startup: " + getTimeSinceStartup()
+				+ ", scheduled invocations: " + invocations.size();
+		LOG.debug(message);
+		return new Response(message, new ArrayList<>(invocations));
+	}
+
+	private Duration getTimeSinceStartup() {
+		return Duration.between(startup, clock.instant());
+	}
 }
--- src/main/java/hello/Response.java
+++ src/main/java/hello/Response.java
@@ -1,32 +1,72 @@
 package hello;
 
+import static java.util.stream.Collectors.toList;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
 public class Response {
 
-    private String message;
-    private Request request;
+	private final String message;
+	private final List<Invocation> invocations;
 
-    public Response(String message, Request request) {
-        this.message = message;
-        this.request = request;
-    }
+	public Response(String message, List<Invocation> invocations) {
+		this.message = message;
+		this.invocations = invocations;
+	}
 
-    public Response() {
-    }
+	public String getMessage() {
+		return this.message;
+	}
 
-    public String getMessage() {
-        return this.message;
-    }
+	public List<String> getInvocations() {
+		return invocations.stream().map(Invocation::toString).collect(toList());
+	}
 
-    public Request getRequest() {
-        return this.request;
-    }
+	public static class Invocation {
+		private final int id;
+		private final Instant timestamp;
+		private final Invocation previousInvocation;
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+		public Invocation(int id, Instant timestamp, Invocation previousInvocation) {
+			this.id = id;
+			this.timestamp = timestamp;
+			this.previousInvocation = previousInvocation;
+		}
 
-    public void setRequest(Request request) {
-        this.request = request;
-    }
+		private Duration getDuration() {
+			return previousInvocation != null ? Duration.between(previousInvocation.timestamp, timestamp) : null;
+		}
 
+		private boolean isShortDuration() {
+			final Duration duration = getDuration();
+			return duration != null ? duration.compareTo(Duration.ofMillis(5)) < 0 : false;
+		}
+
+		private int getCombinedCount() {
+			if (previousInvocation == null) {
+				return 0;
+			}
+			if (this.isShortDuration()) {
+				return 1 + previousInvocation.getCombinedCount();
+			}
+			return 0;
+		}
+
+		@Override
+		public String toString() {
+			final Duration duration = getDuration();
+			final String durationText;
+			if (duration == null) {
+				durationText = "initial invocation";
+			} else if (isShortDuration()) {
+				durationText = "~ 0 (" + duration + ")";
+			} else {
+				durationText = "" + duration;
+			}
+			return "#" + id + ": " + timestamp + ", combined count: " + getCombinedCount() + ", duration: "
+					+ durationText;
+		}
+	}
 }
